67969a3f7f29657e77027c8ad96cf358abef3bbe
João Gabriel Nascimento da Silva <jgabriel10x@gmail.com>
2021-09-14 08:24:34 -0300

    errinho arrumado

--- src/br/com/alura/jdbc/controller/ProdutoController.java
+++ src/br/com/alura/jdbc/controller/ProdutoController.java
@@ -1,13 +1,20 @@
 package br.com.alura.jdbc.controller;
 
+import java.sql.Connection;
 import java.util.List;
 
 import br.com.alura.jdbc.dao.ProdutoDAO;
+import br.com.alura.jdbc.factory.ConnectionFactory;
 import br.com.alura.jdbc.modelo.Produto;
 
 public class ProdutoController {
 	
 	private ProdutoDAO produtoDAO;
+	
+	public ProdutoController() {
+		Connection connection = new ConnectionFactory().recuperarConexao();
+		this.produtoDAO = new ProdutoDAO(connection);
+	}
 
 	public void deletar(Integer id) {
 		this.produtoDAO.deletar(id);
